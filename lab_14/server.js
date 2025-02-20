a575c236e7f3b89a57318f6ee05a483307e88329
jeshurun Olaide <sketchtitan7@gmail.com>
2025-02-20 11:28:27 +0000

    using the values

--- lab_14/server.js
+++ lab_14/server.js
@@ -8,8 +8,21 @@ app.set('view engine','ejs');
 
 //use res.render to load up an ejs viw fil
 // index page
-app.get('/',function(req, res){
-    res.render('pages/index');
+//app.get('/',function(req, res){
+ //   res.render('pages/index');
+//});
+app.get('/', function(req,res){
+    var drinks= [
+        {name: 'Bloody Mary', drunknes:3},
+        {name: 'Martini', drunknes:5},
+        {name: 'Scotch', drunknes:10},
+    ]
+    var tagline= "any code of your own that you havent looked at for six or mre months might as well have been written by someone else.";
+
+    res.render('pages/index',{
+        drinks: drinks,
+        tagline: tagline
+    });
 });
 
 //about page 
@@ -18,4 +31,4 @@ app.get('/about',function(req, res){
 });
 
 app.listen(8080);
-console.log('8080 is the mgic port');
+console.log('8080 is the magic port');
